2e96a9f5d435d2b7a2faddcb88ee18fc465da71a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-21 17:25:59 +0530

    setting up hono redis in backend for route and setting up env

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,29 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { env } from 'hono/adapter';
+import { Redis } from "@upstash/redis";
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath("/api");
 
+type EnvConfig = {
+    UPSTASH_REDIS_REST_URL: string
+    UPSTASH_REDIS_REST_TOKEN: string
+}
+
 app.get('/search', (c) => {
+
+    const { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } = env<EnvConfig>(c);
+
+    // initiate redis db
+    const redis = new Redis({
+        url: UPSTASH_REDIS_REST_URL!,
+        token: UPSTASH_REDIS_REST_TOKEN!
+    })
+
+
+
     return c.json({
         message: 'Hello Next.js!',
     });
